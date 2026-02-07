660e513dfaa96ce4bf9b101e257b2657f7b1373c
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2026-02-07 23:41:30 +0200

    feat: add ChallengesSec component to showcase agricultural challenges

---
+++ src/ui/ChallengesSec.tsx
@@ -0,0 +1,71 @@
+// const ChallengesSec = () => {
+//   return (
+//     <section className="px-8 py-20 md:px-12 lg:px-24">
+//       <div className="text-center">
+//         <p className="text-dark text-4xl font-bold">Challenges</p>
+//         <p className="my-2 font-semibold text-gray-600">
+//           There are lots of factors , making agriculture harder everyday
+//         </p>
+//       </div>
+//       <div className="grid justify-between grid-cols-4 md:grid-cols-7">
+//         {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+//           <img src={`/images/challenge${i}.avif`} key={i} alt="challenge" />
+//         ))}
+//       </div>
+//     </section>
+//   );
+// };
+
+import Slider from "react-slick";
+import useWindowSize from "../hooks/useWindowSize";
+
+// export default ChallengesSec;
+
+const ChallengesSec = () => {
+  const { width } = useWindowSize();
+  const settings = {
+    dots: false,
+    arrows: false,
+    speed: 400,
+    slidesToShow:
+      width > 1024
+        ? 7
+        : width > 768
+          ? 5
+          : width > 650
+            ? 4
+            : width > 425
+              ? 3
+              : 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 768, settings: { slidesToShow: 1 } },
+    ],
+  };
+  return (
+    <section className="px-8 py-20 md:px-12 lg:px-24">
+      {/* Title */}
+      <div className="text-center">
+        <p className="text-dark text-4xl font-bold">Challenges</p>
+        <p className="my-2 font-semibold text-gray-600">
+          There are lots of factors, making agriculture harder everyday
+        </p>
+      </div>
+
+      {/* Challenges Images */}
+      <Slider {...settings} className="features-slider">
+        {[1, 2, 3, 4, 5, 6, 7].map((i) => (
+          <img
+            src={`/images/challenge${i}.avif`}
+            alt="challenge"
+            className="transition-all hover:translate-y-5"
+          />
+        ))}
+      </Slider>
+    </section>
+  );
+};
+
+export default ChallengesSec;
