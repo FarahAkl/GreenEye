3d5a27eb4aaa814f504c8f614f14530682e295ed
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2026-02-08 02:08:15 +0200

    fix: add border to Card component for improved styling

--- src/ui/Card.tsx
+++ src/ui/Card.tsx
@@ -6,7 +6,7 @@ interface IProps {
 
 const Card = ({ icon, label, describtion }: IProps) => {
   return (
-    <div className="flex w-full flex-col items-center justify-center gap-3 overflow-hidden rounded-lg bg-white px-10 py-12 text-center shadow-sm transition duration-300 hover:hover:shadow-md">
+    <div className="flex w-full flex-col items-center justify-center gap-3 overflow-hidden rounded-lg border border-gray-100 bg-white px-10 py-12 text-center shadow-sm transition duration-300 hover:hover:shadow-md">
       <div className="relative h-12 w-12 rounded-tl-2xl rounded-tr-lg rounded-br-2xl rounded-bl-lg bg-[#E4F2E8]">
         <img src={icon} alt="icon" className="absolute -top-3 -left-3" />
       </div>
