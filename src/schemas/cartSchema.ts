0f5ebbadf9b11d258fa796cce35b20c015816247
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2026-02-14 04:24:34 +0200

    feat: implement cart schema and API services for cart operations

---
+++ src/schemas/cartSchema.ts
@@ -0,0 +1,44 @@
+import { z } from "zod";
+
+export const itemSchema = z.object({
+  id: z.number(),
+  productId: z.number(),
+  productName: z.string(),
+  productImage: z.string(),
+  unitPrice: z.number(),
+  quantity: z.number(),
+  totalPrice: z.number(),
+  availableStock: z.number(),
+});
+
+export const cartResponseSchema = z.object({
+  isSuccess: z.boolean(),
+  message: z.string().nullable(),
+  data: z.object({
+    id: z.number,
+    items: z.array(itemSchema),
+    totalPrice: z.number(),
+    totalItems: z.number(),
+  }),
+});
+
+export const deleteCartSchema = z.object({
+  isSuccess: z.boolean(),
+  message: z.string().nullable(),
+  data: z.object().nullable(),
+});
+
+export const addItemToCartSchema = z.object({
+  productId: z.number(),
+  quantity: z.number(),
+});
+
+export const updateItemInCartSchema = z.object({
+  quantity: z.number(),
+});
+
+export type itemT = z.infer<typeof itemSchema>;
+export type cartResponseT = z.infer<typeof cartResponseSchema>;
+export type deleteCartT = z.infer<typeof deleteCartSchema>;
+export type addItemToCartT = z.infer<typeof addItemToCartSchema>;
+export type updateItemInCartT = z.infer<typeof updateItemInCartSchema>
---
+++ src/services/apiCart.ts
@@ -0,0 +1,65 @@
+import { isAxiosError } from "axios";
+import axiosInstance from "./axiosInstance";
+import { cartResponseSchema, type addItemToCartT, type updateItemInCartT } from "../schemas/cartSchema";
+
+export const getCart = async () => {
+  const res = await axiosInstance.get("/api/marketplace/Cart");
+  return res.data;
+};
+
+export const deleteCart = async () => {
+  const res = await axiosInstance.delete("/api/marketplace/Cart");
+  return res.data;
+};
+
+export const addItemToCart = async (data: addItemToCartT) => {
+  try {
+    const res = await axiosInstance.post(
+      `/api/marketplace/Cart/add-items`,
+      data,
+    );
+    const validatedRes = cartResponseSchema.parse(res.data);
+    return validatedRes;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      const message =
+        err.response?.data?.error ||
+        err.response?.data?.message ||
+        err.message ||
+        "Something went wrong";
+
+      throw new Error(message);
+    }
+    throw err;
+  }
+};
+
+export const updateProduct = async ({
+  cartItemId,
+  data,
+}: {
+  cartItemId: string;
+  data: updateItemInCartT;
+}) => {
+  try {
+    const res = await axiosInstance.put(
+      `/api/marketplace/Cart/items/${cartItemId}`,
+      data,
+    );
+    return res.data;
+  } catch (err) {
+    if (isAxiosError(err)) {
+      throw new Error(
+        err.response?.data?.message || "Failed to update item",
+      );
+    }
+    throw err;
+  }
+};
+
+export const deleteItemInCart = async (cartItemId:string) => {
+  const res = await axiosInstance.delete(
+    `/api/marketplace/Cart/items/${cartItemId}`,
+  );
+  return res.data;
+};
