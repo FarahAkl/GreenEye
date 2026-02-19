9ea55b456af0231582a74afa23db31f25aeb8a67
FarahAkl <120726488+FarahAkl@users.noreply.github.com>
2026-02-20 01:47:09 +0200

    feat: add background image and restructure AuthLayout component for improved layout

--- src/ui/AuthLayout.tsx
+++ src/ui/AuthLayout.tsx
@@ -1,10 +1,27 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "./Navbar";
 
 const AuthLayout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="flex min-h-screen flex-col">
+      <Navbar />
+
+      <div className="relative flex flex-1 items-center justify-center">
+        <img
+          src="/images/authBG.png"
+          alt="background"
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+
+        <div className="relative z-10 mx-3 my-5 flex min-h-full">
+          <div className="rounded-2xl bg-white px-8 py-6 shadow-2xl">
+            <Outlet />
+          </div>
+        </div>
+
+        <div id="modal-root"></div>
+      </div>
+    </div>
   );
 };
 
